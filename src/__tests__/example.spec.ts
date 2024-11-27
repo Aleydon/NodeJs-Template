4bfda5ffabf0a62e6e581563152db400061bf02b
Aleydon <aleydonroberto@gmail.com>
2024-11-27 16:31:22 -0300

    FIX: Adjusting relative paths in Jest

---
+++ src/__tests__/example.spec.ts
@@ -0,0 +1,16 @@
+import { UserModel } from 'models/user.model';
+
+const sum = (numberA: number, numberB: number) => {
+	return numberA + numberB;
+};
+
+describe('User Model', () => {
+	it('should check if the name passed in the User model is Aleydon', () => {
+		const user = new UserModel('Aleydon', 29);
+		expect(user.name).toBe('Aleydon');
+	});
+	it('should sum two numbers correctly', () => {
+		const result = sum(1, 2);
+		expect(result).toBe(3);
+	});
+});
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express, { type Request, type Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 
-const port = 3001;
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3001;
 
 app.get('/', (_req: Request, res: Response) => {
 	res.send('Hello World!');
 });
 
-app.listen(port, () => {
-	console.log(`Server runing on localhost://${port}`);
+app.listen(PORT, () => {
+	console.log(`Server runing on localhost://${PORT} }`);
 });
